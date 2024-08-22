61526fd6e52a4654d77b5f6393d84f6337af869d
Bruce Kristelijn <bruce.kristelijn@enovationgroup.com>
2024-08-22 14:27:25 +0200

    chore: Update Prisma schema and models, add Predictions model
    Note: This commit includes changes to the Prisma schema and models, adding a new model for Predictions and updating the User and Pool models.

--- src/views/MatchView.vue
+++ src/views/MatchView.vue
@@ -1,21 +1,28 @@
 <template>
     <div class="flex flex-col">
-        <div class="card bg-base-100 w-80 shadow-xl">
+        <div class="card bg-base-100 w-3/4 shadow-xl self-center">
             <div v-if="loading" class="card-body">
                 <div class="skeleton h-32 w-full"></div>
                 <div class="skeleton h-4 w-full"></div>
                 <div class="skeleton h-4 w-full"></div>
             </div>
 
-            <div v-else class="card-body justify-center">
+            <div v-else class="card-body justify-center items-center">
                 <!-- Competition  -->
-                <div class="flex flex-row gap-2 justify-center">
+                <div class="flex flex-row gap-2 justify-center items-center">
                     <!-- <img :src="match.area.flag" alt="Competition logo" class="w-10 h-10" /> -->
                     <img :src="match.competition.emblem" alt="Competition logo" class="w-10 h-10" />
 
                     <h2 class="card-title">
                         {{ match.competition.name }}
                     </h2>
+                </div>
+
+                <!-- Location card -->
+                <div class="badge badge-primary">
+                    <p class="font-sm">
+                        {{ match.venue }}
+                    </p>
                 </div>
 
                 <!-- Countdown -->
@@ -28,7 +35,7 @@
                 </div>
 
                 <!-- Crests & Scorecards -->
-                <div class="card-body justify-center">
+                <div class="card-body justify-center badge">
                     <div class="flex flex-row gap-2 justify-center">
                         <img :src="match.homeTeam.crest" alt="Home team logo" class="w-10 h-10" />
                         <h3 v-if="match_state == 0">VS</h3>
@@ -39,36 +46,121 @@
                         <img :src="match.awayTeam.crest" alt="Away team logo" class="w-10 h-10" />
                     </div>
                 </div>
-            </div>
 
+                <!-- Prediction card -->
+                <div class="card bg-base-200 w-3/4 shadow-xl text-center mt-5">
+                    <div class="card-body my-0">
+                        <h1>Voorspelling</h1>
+                        <h2>Stand bij rust</h2>
 
+                        <!-- Form sucess display -->
+                        <transition name="list">
+                            <div class="alert alert-success mb-2 w-3/4 mx-auto p-2" v-if="showPredictionFormSuccess">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 shrink-0 stroke-current"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <span>
+                                    Prediction saved!
+                                </span>
+                            </div>
+                        </transition>
 
+                        <!-- Form error display -->
+                        <TransitionGroup name="list" tag="div">
+                            <div v-for="error in predictionFormErrors" role="alert" :key="error"
+                                class="alert alert-error mb-2 w-3/4 mx-auto p-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 shrink-0 stroke-current"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <span>
+                                    {{ error }}
+                                </span>
+                            </div>
+                        </TransitionGroup>
 
-            {{ match }}
+                        <div class="gap-2 justify-center flex flex-row items-center mx-10">
 
+                            <label class="form-control flex flex-col w-full max-w-xs ">
+                                Home
+                                <input type="number" placeholder="" class="input input-bordered w-full my-1"
+                                    v-model="prediction.halftime.home" @change="validateMatchPrediction" :disabled="match_state != 0" />
+                            </label>
+                            <span class="w-1/2">-</span>
+                            <label class="form-control flex flex-col w-full max-w-xs">Away
+                                <input type="number" placeholder="" class="input input-bordered w-full my-1"
+                                    v-model="prediction.halftime.away" @change="validateMatchPrediction" :disabled="match_state != 0" />
+                            </label>
+                        </div>
 
-            <!-- <div class="card bg-base-100 w-80 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">Other matches</h2>
-                    <MatchesComponent />
+                        <h2>Eindstand</h2>
+                        <div class="gap-2 justify-center flex flex-row items-center mx-10">
+
+                            <label class="form-control flex flex-col w-full max-w-xs">
+                                Home
+                                <input type="number" placeholder="" class="input input-bordered w-full my-1"
+                                    v-model="prediction.final.home" @change="validateMatchPrediction" :disabled="match_state != 0" />
+                            </label>
+                            <span class="w-1/2">-</span>
+                            <label class="form-control flex flex-col w-full max-w-xs">
+                                Away
+                                <input type="number" placeholder="" class="input input-bordered w-full my-1"
+                                    v-model="prediction.final.away" @change="validateMatchPrediction" :disabled="match_state != 0" />
+                            </label>
+                        </div>
+
+                        <!-- Submit button -->
+                        <button class="btn btn-primary mt-4 w-3/4 mx-auto" :disabled="isMatchPredictionLoading"
+                            @click="saveMatchPrediction" v-if="match_state == 0">
+                            Save
+                            <span class="loading loading-spinner loading-md" v-if="isMatchPredictionLoading"></span>
+                        </button>
+
+                        <!-- Score card -->
+                         <div v-else >
+                            Prediction score: <br>
+                            <ScoreBadgeComponent score="30" />
+                            <ScoreBadgeComponent score="-30" /> 
+                        </div>
+
+                    </div>
                 </div>
-            </div> -->
+            </div>
+            <!-- {{ match }} -->
         </div>
     </div>
 </template>
 
 <script>
 import MatchesComponent from '../components/MatchesComponent.vue'
+import ScoreBadgeComponent from '../components/ScoreBadgeComponent.vue';
 
 export default {
     components: {
-        MatchesComponent
+        MatchesComponent,
+        ScoreBadgeComponent
     },
     data() {
         return {
             loading: true,
             time_left: "",
-            match_state: 0 // 0 = planned, 1 = live, 2 = finished
+            match_state: 0, // 0 = planned, 1 = live, 2 = finished
+            isMatchPredictionLoading: false,
+            predictionFormErrors: [],
+            showPredictionFormSuccess: false,
+            prediction: {
+                halftime: {
+                    home: 0,
+                    away: 0
+                },
+                final: {
+                    home: 0,
+                    away: 0
+                },
+            }
         }
     },
     computed: {
@@ -99,6 +191,40 @@ export default {
 
                 return `${days}d ${hours}h ${minutes}m ${seconds}s`;
             }
+        },
+        validateMatchPrediction() {
+            this.predictionFormErrors = [];
+            const values = [this.prediction.halftime.home, this.prediction.halftime.away, this.prediction.final.home, this.prediction.final.away]
+
+            // All numeric
+            if (values.some(isNaN)) {
+                this.predictionFormErrors.push("All fields must be numeric")
+            }
+
+            // Check if final score is higher than halftime score
+            if (this.prediction.final.home < this.prediction.halftime.home || this.prediction.final.away < this.prediction.halftime.away) {
+                this.predictionFormErrors.push("Final score must be higher than halftime score");
+            }
+
+            // Check if any score is negative
+            if (values.some(v => v < 0)) {
+                this.predictionFormErrors.push("Scores cannot be negative")
+            }
+        },
+        async saveMatchPrediction() {
+            if (this.predictionFormErrors.length > 0) return;
+
+            this.isMatchPredictionLoading = true;
+
+            // Await 2 seconds
+            await new Promise(resolve => setTimeout(resolve, 2000))
+
+            
+            this.isMatchPredictionLoading = false;
+            this.showPredictionFormSuccess = true;
+
+            await new Promise(resolve => setTimeout(resolve, 2000))
+            this.showPredictionFormSuccess = false;
         }
     },
     async created() {
@@ -109,9 +235,32 @@ export default {
 
         // Setup time left countdown
         this.time_left = this.get_time_left()
+
+        // Await 2 seconds
         setInterval(() => {
             this.time_left = this.get_time_left()
         }, 1000)
     }
 }
 </script>
+
+<style scoped>
+.list-move,
+/* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.2s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+    position: absolute;
+}
+</style>
