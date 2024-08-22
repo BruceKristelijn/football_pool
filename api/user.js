cc47f57c55dd0889aeffadd56c6fb9afe7cf8585
Bruce Kristelijn <bruce.kristelijn@gmail.com>
2024-08-22 06:56:55 +0200

    chore: Update data in prisma orm

--- api/user.js
+++ api/user.js
@@ -9,12 +9,12 @@ export default async function handler(request, response) {
     const newUser = await prisma.user.create({
         data: {
             name: 'Elliott',
-            email: 'xelliottx@example-user.com',
+            email: 'xelldsfsdiottx@example-user.com',
             image: ""
         },
     });
 
     const users = await prisma.user.findMany();
 
-    return response.status(200).json({ message: rows });
+    return response.status(200).json({ message: newUser });
 }
