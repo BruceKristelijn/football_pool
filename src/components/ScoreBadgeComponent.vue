d839f769820577be621e5a8eb46089be6f52a6b3
Bruce Kristelijn <bruce.kristelijn@enovationgroup.com>
2024-08-22 14:27:15 +0200

    chore: Add ScoreBadgeComponent.vue for displaying score badges

---
+++ src/components/ScoreBadgeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="badge p-4" :class="class">
+        <p class="font-bold text-lg text-base-100">
+            <span v-if="score > 0">+</span>
+            <span v-else>-</span>
+            {{ Math.abs(score) }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['score'],
+    computed: {
+        class() {
+            return {
+                'badge-success': this.score > 0,
+                'badge-error': this.score < 0
+            }
+        }
+    }
+}
+</script>
